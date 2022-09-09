0788488f1fe8927b0454e6a80d5069a9b3b31c85
sfjbssept <sfjbssept@gmail.com>
2022-09-09 07:06:21 +0000

    initial commit for productMgmtSystem

---
+++ ProductsManagementSystem/src/main/java/com/product/ProductsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ ProductsManagementSystem/src/main/java/com/product/entity/Product.java
@@ -0,0 +1,5 @@
+package com.product.entity;
+
+public class Product {
+
+}
