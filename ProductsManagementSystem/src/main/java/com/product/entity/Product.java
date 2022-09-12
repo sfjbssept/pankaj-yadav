67b82c1271e666ede2ed08bc19700dae51e95130
sfjbssept <sfjbssept@gmail.com>
2022-09-12 04:14:44 +0000

    updated application properties

--- ProductsManagementSystem/src/main/java/com/product/entity/Product.java
+++ ProductsManagementSystem/src/main/java/com/product/entity/Product.java
@@ -1,5 +1,74 @@
 package com.product.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String productName;
+	private String productBrand;
+	private String productCategory;
+	private Integer productPrice;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getProductBrand() {
+		return productBrand;
+	}
+
+	public void setProductBrand(String productBrand) {
+		this.productBrand = productBrand;
+	}
+
+	public String getProductCategory() {
+		return productCategory;
+	}
+
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Integer getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(Integer productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public Product(Integer id, String productName, String productBrand, String productCategory, Integer productPrice) {
+		super();
+		this.id = id;
+		this.productName = productName;
+		this.productBrand = productBrand;
+		this.productCategory = productCategory;
+		this.productPrice = productPrice;
+	}
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
