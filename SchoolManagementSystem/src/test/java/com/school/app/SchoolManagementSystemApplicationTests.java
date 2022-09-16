e39ec8928945c49a1424ee6aff4160a2fd46550c
sfjbssept <sfjbssept@gmail.com>
2022-09-16 04:58:29 +0000

    added microservice project

---
+++ SchoolManagementSystem/src/test/java/com/school/app/SchoolManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
