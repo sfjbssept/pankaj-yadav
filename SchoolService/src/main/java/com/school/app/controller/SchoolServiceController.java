0cf6b8c260cac23b8028909eb6848a98826e77c5
sfjbssept <sfjbssept@gmail.com>
2022-09-16 05:26:03 +0000

    modified schoolController

--- SchoolService/src/main/java/com/school/app/controller/SchoolServiceController.java
+++ SchoolService/src/main/java/com/school/app/controller/SchoolServiceController.java
@@ -1,8 +1,15 @@
 package com.school.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/schooldetail")
 public class SchoolServiceController {
+	
+	@Autowired
+	RestTemplate restTemplate;
 
 }
