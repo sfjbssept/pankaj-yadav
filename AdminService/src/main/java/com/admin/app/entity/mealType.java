cb4a781ed156b15c9311576f2368f273d06d3115
sfjbssept <sfjbssept@gmail.com>
2022-09-26 07:36:29 +0000

    removed alone AdminService

--- AdminService/src/main/java/com/admin/app/entity/mealType.java
+++
@@ -1,8 +0,0 @@
-package com.admin.app.entity;
-
-public enum mealType {
-	NONE,
-	VEG,
-	NONVEG
-
-}
--- AdminService/src/main/java/com/admin/app/repo/IFlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.admin.app.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.admin.app.entity.Flight;
-
-public interface IFlightRepository extends JpaRepository<Flight, Integer>{
-
-}
