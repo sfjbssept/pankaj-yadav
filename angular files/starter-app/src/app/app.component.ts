efabc21820fb9387f312f39ad580decd44bee6d3
sfjbssept <sfjbssept@gmail.com>
2022-10-10 05:47:53 +0000

    angular project added

---
+++ angular files/starter-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  username:String = "";
+  age:number = 0;
+  title = 'Hello-world';
+  desc:String = "AngularJS extends HTML with new attributes,AngularJS is perfect for Single Page Applications (SPAs).AngularJS is easy to learn."
+
+  save(){
+    console.log("the user's name is "+this.username+" whose age is: "+this.age);
+  }
+}
