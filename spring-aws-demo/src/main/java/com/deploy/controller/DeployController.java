3c4c9282c35e65d02c059fa75a9ff5c026041247
sfjbssept <sfjbssept@gmail.com>
2022-11-02 13:29:03 +0000

    modifyig UI App and added deploy spring boot App

--- spring-aws-demo/src/main/java/com/deploy/controller/DeployController.java
+++ spring-aws-demo/src/main/java/com/deploy/controller/DeployController.java
@@ -1,6 +1,7 @@
 package com.deploy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class DeployController {
 	@GetMapping("/hello")
 	public String helloWorld() {
 		return "Hello My Name Is Pankaj Yadav";
+	}
+	
+	@GetMapping("/{name}")
+	public String helloName(@PathVariable String name) {
+		return "Hello My Name Is "+name;
 	}
 
 }
