a4bc57e3df87ebb413e0aa656c68d89d114baae7
sfjbssept <sfjbssept@gmail.com>
2022-11-21 05:42:15 +0000

    removed unnecessary components

--- angular files/flight-app-ui-nvnt/src/app/app-routing.module.ts
+++ angular files/flight-app-ui-nvnt/src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AirlineComponent } from './components/airline/airline.component';
 import { BookingMgtComponent } from './components/booking-mgt/booking-mgt.component';
-import { CaradminComponent } from './components/caradmin/caradmin.component';
-import { CarbrokerComponent } from './components/carbroker/carbroker.component';
-import { CarleaseComponent } from './components/carlease/carlease.component';
 import { FlighcreateComponent } from './components/flighcreate/flighcreate.component';
 import { FlightadminComponent } from './components/flightadmin/flightadmin.component';
 import { SearchflightComponent } from './components/searchflight/searchflight.component';
@@ -13,8 +10,6 @@ import { LoginComponent } from './components/login/login.component';
 import Airline from './entity/Airline';
 
 const routes: Routes = [
-{path:"carbroker", component:CarbrokerComponent},
-{path:"carlease", component:CarleaseComponent},
 {path:"airline", component:AirlineComponent},
 {path:"flightcreate", component:FlighcreateComponent},
 {path:"flightadmin", component:FlightadminComponent},
--- angular files/flight-app-ui-nvnt/src/app/app.module.ts
+++ angular files/flight-app-ui-nvnt/src/app/app.module.ts
@@ -5,9 +5,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CaradminComponent } from './components/caradmin/caradmin.component';
-import { CarbrokerComponent } from './components/carbroker/carbroker.component';
-import { CarleaseComponent } from './components/carlease/carlease.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FlightadminComponent } from './components/flightadmin/flightadmin.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -21,9 +18,6 @@ import { SearchflightComponent } from './components/searchflight/searchflight.co
 @NgModule({
   declarations: [
     AppComponent,
-    CaradminComponent,
-    CarbrokerComponent,
-    CarleaseComponent,
     NavbarComponent,
     FlightadminComponent,
     AirlineComponent,
